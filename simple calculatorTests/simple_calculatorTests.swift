e8fe7b903d7c9ecac4d5ec6ec2e5ed9b3ee23f53
Md Imam Hossain <imamagun94@gmail.com>
2021-11-01 10:09:41 -0700

    feat: first commit

---
+++ simple calculatorTests/simple_calculatorTests.swift
@@ -0,0 +1,34 @@
+//
+//  simple_calculatorTests.swift
+//  simple calculatorTests
+//
+//  Created by Imam Hossain on 1/11/21.
+//  Copyright © 2021 NewAgeDevs. All rights reserved.
+//
+
+import XCTest
+@testable import simple_calculator
+
+class simple_calculatorTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
